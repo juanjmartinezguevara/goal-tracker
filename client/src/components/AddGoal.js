703a0a6088fa93fbf47b2ce7eda2db6f41a5a4e0
Juan Martinez Guevara <juanjmartinezguevara@users.noreply.github.com>
2021-04-12 23:52:02 -0500

    Add AddGoal submission form

--- client/src/components/AddGoal.js
+++ client/src/components/AddGoal.js
@@ -1,10 +1,40 @@
 import React from "react";
 
 function AddGoal(props) {
+  const handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    alert("Form was successfully submitted!");
+    event.preventDefault();
+  };
+
   return (
     <div className="bg-color">
       <div className="margin-main">
-        <h1>Add goal feature coming soon!</h1>
+        <form>
+          <label>
+            Goal Description:
+            <input type="text" />
+          </label>
+          <div>
+            <label>
+              <input value="daily" type="radio"></input>Daily
+            </label>
+            <label>
+              <input value="weekly" type="radio"></input>Weekly
+            </label>
+            <label>
+              <input value="biweekly" type="radio"></input>Biweekly
+            </label>
+            <label>
+              <input value="monthly" type="radio"></input>Monthly
+            </label>
+          </div>
+          <br />
+          <input type="submit" value="Submit" onClick={handleSubmit}/>
+        </form>
       </div>
     </div>
   );
